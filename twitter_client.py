527611f776d493a6b795e424db42b07081698e7e
giwing <86605626+giwing3@users.noreply.github.com>
2025-06-15 14:04:29 +0700

    Create twitter_client.py

---
+++ twitter_client.py
@@ -0,0 +1,5 @@
+import tweepy from config import TWITTER_API_KEY, TWITTER_API_SECRET, TWITTER_ACCESS_TOKEN, TWITTER_ACCESS_SECRET
+
+auth = tweepy.OAuth1UserHandler( TWITTER_API_KEY, TWITTER_API_SECRET, TWITTER_ACCESS_TOKEN, TWITTER_ACCESS_SECRET ) twitter_api = tweepy.API(auth)
+
+def post_tweet(text): twitter_api.update_status(status=text)
