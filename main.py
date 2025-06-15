d1de349e8c371ac41193cf769ebd703c097af035
Giwing <wawangiwing29@gmail.com>
2025-06-15 16:48:20 +0700

    WIP

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
 from gemini_client import generate_tweet
 from telegram_bot import send_for_approval
+
+if __name__ == "__main__":
+    tweet = generate_tweet()
+    print("Generated Tweet:", tweet)
+    send_for_approval(tweet)
